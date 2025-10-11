bfd754d23fdfcf1b3ae3584dd3df63932dce7fd9
fujitani sora <fujitanisora0414@gmail.com>
2025-10-11 11:32:42 +0900

    sync

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
@@ -7,15 +7,15 @@ config.automatically_reload_config = false
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
 -- set leader
-config.leader = { key = 'a',mods="CMD", timeout_milliseconds = 2000 }
+config.leader = { key = "a", mods = "CMD", timeout_milliseconds = 2000 }
 
 -- font size
 config.font_size = 14.0
-wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+wezterm.font("Hack Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 
 -- right status
-wezterm.on('update-right-status', function(window)
-  window:set_right_status(window:active_workspace())
+wezterm.on("update-right-status", function(window)
+	window:set_right_status(window:active_workspace())
 end)
 
 -- use jp lang
@@ -29,7 +29,8 @@ config.macos_window_background_blur = 20
 -- config.color_scheme = 'Aci (Gogh)'
 -- config.color_scheme = 'Ef-Deuteranopia-Dark'
 -- config.color_scheme = 'ENCOM'
-config.color_scheme = 'Ef-Night'
+config.color_scheme = "Ef-Night"
+-- config.color_scheme = 'Blue Matrix'
 
 -- tab bar
 config.use_fancy_tab_bar = false
@@ -37,95 +38,97 @@ config.tab_bar_at_bottom = false
 config.window_decorations = "RESIZE"
 config.show_new_tab_button_in_tab_bar = false
 config.tab_bar_at_bottom = false
-config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
-},
---
-wezterm.on("format-tab-title", function(tab)
-  local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
-  local background = scheme.background
-  local foreground = scheme.foreground
+config.colors =
+	{
+		tab_bar = {
+			inactive_tab_edge = "none",
+		},
+	},
+	--
+	wezterm.on("format-tab-title", function(tab)
+		local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
+		local background = scheme.background
+		local foreground = scheme.foreground
 
-  if tab.is_active then
-    foreground = scheme.brights[8]
-  end
+		if tab.is_active then
+			foreground = scheme.brights[8]
+		end
 
-  return {
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-  }
-end)
+		return {
+			{ Background = { Color = background } },
+			{ Foreground = { Color = foreground } },
+		}
+	end)
 
 -- tab bar color asnc color_scheme
 config.colors = {
-  tab_bar = {
-    background = wezterm.color.get_builtin_schemes()[config.color_scheme].background,
-  },
+	tab_bar = {
+		background = wezterm.color.get_builtin_schemes()[config.color_scheme].background,
+	},
 }
 
 config.keys = {
-  -- reload
-  {
-    key = 'r',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ReloadConfiguration,
-  },
-  -- close tab
-  {
-    key = "w",
-    mods = "CMD",
-    action = act.CloseCurrentPane { confirm = true },
-  },
-  -- pane split
-  {
-    key = ",",
-    mods = "CMD",
-    action = act { SplitVertical = { domain = "CurrentPaneDomain" } },
-  },
-  {
-    key = ".",
-    mods = "CMD",
-    action = act { SplitHorizontal = { domain = "CurrentPaneDomain" } },
-  },
-  {
-    key = 'LeftArrow',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection('Left')
-  },
-  {
-    key = 'RightArrow',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection('Right')
-  },
-  {
-    key = 'UpArrow',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection('Up')
-  },
-  {
-    key = 'DownArrow',
-    mods = 'CMD',
-    action = act.ActivatePaneDirection('Down')
-  },
-  -- workspace
-  {
-    key = 'LeftArrow',
-    mods = 'ALT',
-    action = act.SwitchWorkspaceRelative(1)
-  },
-  {
-    key = 'RightArrow',
-    mods = 'ALT',
-    action = act.SwitchWorkspaceRelative(-1)
-  },
-  {
-    key = '9',
-    mods = 'ALT',
-    action = act.ShowLauncherArgs {
-      flags = 'FUZZY|WORKSPACES'
-    }
-  },
+	-- reload
+	{
+		key = "r",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ReloadConfiguration,
+	},
+	-- close tab
+	{
+		key = "w",
+		mods = "CMD",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
+	-- pane split
+	{
+		key = ",",
+		mods = "CMD",
+		action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = ".",
+		mods = "CMD",
+		action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "CMD",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	-- workspace
+	{
+		key = "LeftArrow",
+		mods = "ALT",
+		action = act.SwitchWorkspaceRelative(1),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT",
+		action = act.SwitchWorkspaceRelative(-1),
+	},
+	{
+		key = "9",
+		mods = "ALT",
+		action = act.ShowLauncherArgs({
+			flags = "FUZZY|WORKSPACES",
+		}),
+	},
+	{ key = "Enter", mods = "SHIFT", action = act.SendString("\n") },
 }
 return config, {}
